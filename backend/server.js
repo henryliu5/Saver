182147837ebe5dcb0e7b4a55023f00f830bb7c7e
Henry <henry.liu.texas@gmail.com>
2020-04-27 13:41:06 -0500

    Initial backend commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+
+const uri = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@saver-cluster-d2gru.mongodb.net/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+async function main(){
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main();
