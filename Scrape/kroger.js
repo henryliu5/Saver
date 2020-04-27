586f50b4701f9ec0e11554c116f4d513c0a8ff36
Henry <henry.liu.texas@gmail.com>
2020-04-27 10:49:28 -0500

    Add Kroger API Integration
    Imports base64 and dotenv npm packages to securely store and access OAuth client credentials. Creates a series of API calls to get authorized access tokens, find location IDs, and get product information. Updated format for genericObj to include retailer name and handle errors.

---
+++ Scrape/kroger.js
@@ -0,0 +1,112 @@
+const fetch = require('node-fetch');
+let base64 = require('base-64');
+require('dotenv').config()
+
+const apiBaseUrl = process.env.KROGER_API_BASE_URL;
+const oauth2BaseUrl = process.env.KROGER_OAUTH2_BASE_URL;
+const clientId = process.env.KROGER_CLIENT_ID;
+const clientSecret = process.env.KROGER_CLIENT_SECRET;
+var thisZip;
+
+// Gets access token from Kroger Authorization server using client_id and client_secret
+async function getAccessToken() {
+    try {
+        const scope = encodeURIComponent('product.compact');
+        let url = `${oauth2BaseUrl}/token?grant_type=client_credentials&scope=${scope}`
+        let response = await fetch(url,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Basic ${base64.encode(clientId + ":" + clientSecret)}`
+                }
+            });
+        var responseObj = await response.json();
+        return responseObj.access_token;
+    } catch (error) {
+        console.log('Failed to get access token');
+        console.error(error);
+    }
+}
+
+// Get location ID of desired store
+async function getLocations(zipCode) {
+    try {
+        // Use stored access token for location request
+        let accessToken = await getAccessToken();
+        // Filter chains to only Kroger stores
+        let url = `${apiBaseUrl}/v1/locations?filter.zipCode.near=${zipCode}`;
+        let response = await fetch(url, {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        });
+        var responseObj = await response.json();
+        console.log(responseObj)
+        if(responseObj.data.length == 0){
+            console.log("No Kroger locations found near this zip code");
+            return null;
+        }
+        return responseObj.data[0].locationId;
+    } catch (error) {
+        console.log('Failed to get location ID');
+        console.log(responseObj);
+        console.error(error);
+    }
+}
+
+// Get product information for given query and zip - zip used to determine location Id of store
+// TODO enable fetching of product information from multiple stores to get best prices
+// TODO fix case where there are no valid products
+async function getData(query, zipCode) {
+    thisZip = zipCode;
+    let genericObjArr = [];
+    // Get necessary info for api request
+    let locationId = await getLocations(zipCode);
+    // Check to see if there is store in range
+    if (locationId) {
+        let accessToken = await getAccessToken();
+        let url = `${apiBaseUrl}/v1/products?filter.locationId=${locationId}&filter.term=${query}`;
+        let response = await fetch(url, {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        });
+        // Push generic objects from response body into return
+        let responseObj = await response.json();
+        for (let i = 0; i < responseObj.data.length; i++) {
+            genericObjArr.push(getGenericObj(responseObj.data[i], i));
+        }
+    }
+    return genericObjArr;
+}
+
+// Converts product object from Kroger API into our generic object
+function getGenericObj(krogerObj, rank) {
+    console.log(krogerObj)
+    var genericObj = new Object();
+    try{
+        genericObj.retailer = "Kroger";
+        genericObj.productName = krogerObj.description;
+        genericObj.price = krogerObj.items[0].price.regular;
+        genericObj.unitPrice = null;
+        genericObj.inStock = krogerObj.items[0].fulfillment.inStore;
+        genericObj.unit = krogerObj.items[0].size;
+        genericObj.img = null;
+        genericObj.zipCode = thisZip;
+        genericObj.rank = rank;
+    } catch(error){
+        genericObj.error = error;
+        genericObj.originalObj = krogerObj;
+    }
+    return genericObj;
+}
+
+module.exports = { getData };
+
