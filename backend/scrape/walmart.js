70385cceecc327c8897b0b24857645783cab0788
Henry <henry.liu.texas@gmail.com>
2020-05-20 23:08:02 -0500

    Add --no-sandbox to puppeteer for heroku

--- backend/scrape/walmart.js
+++ backend/scrape/walmart.js
@@ -6,17 +6,17 @@ const url = 'https://www.walmart.com/search/?query=';
 //13.60s for 5 products
 //31.63s for 10 products
 
-async function getData (query, reqZip) {
+async function getData(query, reqZip) {
     var result = [];
     //open puppeteer browser
-    const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+    const browser = await puppeteer.launch({ headless: true, defaultViewport: null, args: ['--no-sandbox'] });
     const page = await browser.newPage();
     //set page options (viewport, useragent, and requestInterception)
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.0 Safari/537.36');
-    await page.setViewport({width: 1440, height: 900});
+    await page.setViewport({ width: 1440, height: 900 });
     await page.setRequestInterception(true);
     page.on('request', (req) => {
-        if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+        if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
             req.abort();
         } else {
             req.continue();
@@ -28,15 +28,15 @@ async function getData (query, reqZip) {
     //select all product elements 
     const searchResults = await page.$$('[class="product-title-link line-clamp line-clamp-2 truncate-title"]');
     // Get product URLs from all searchResults
-    const productUrls =[];
-    for(res of searchResults) {
+    const productUrls = [];
+    for (res of searchResults) {
         let productUrl = await page.evaluate(body => body.getAttribute('href'), res);
         productUrls.push('https://www.walmart.com' + productUrl);
     }
     //iterate through product URLs and asynchronously get generic object for each product page
-    for(let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
         try {
-            let productBrowser = await puppeteer.launch({headless: true, defaultViewport: null});
+            let productBrowser = await puppeteer.launch({ headless: true, defaultViewport: null });
             result.push(getItem(productBrowser, productUrls[i], i, reqZip));
         } catch (e) {
             console.log("Failed to create page: \n" + e);
@@ -48,15 +48,15 @@ async function getData (query, reqZip) {
     return result;
 }
 
-async function getItem (browser, curUrl, rank, reqZip) {
+async function getItem(browser, curUrl, rank, reqZip) {
     const page = await browser.newPage();
     //set page options (viewport, useragent, and requestInterception (ignore styling/images))
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.0 Safari/537.36');
-    await page.setViewport({width: 1920, height: 1080});
+    await page.setViewport({ width: 1920, height: 1080 });
     //ignore styling and images to improve performance
     await page.setRequestInterception(true);
     page.on('request', (req) => {
-        if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+        if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
             req.abort();
         } else {
             req.continue();
@@ -76,11 +76,11 @@ async function checkZipCode(page, reqZip) {
     await page.waitForSelector('div[class="p_a p_ao p_v p_af ap_b"] > button[class="g_a ap_c g_c"]');
     await page.click('button[class="g_a ap_c g_c"]');
     await page.waitForSelector('[class="j_a zipCode j_b"]');
-    
+
     //check if zip needs to be updated
     var zipCodeElem = await page.$('span[class="j_a zipCode j_b"]');
     let curZipCode = await page.evaluate(el => el.textContent, zipCodeElem);
-    if(curZipCode != reqZip) {
+    if (curZipCode != reqZip) {
         await updateZipCode(page, reqZip);
     }
 }
@@ -90,14 +90,14 @@ async function updateZipCode(page, reqZip) {
     await page.waitForSelector('button[id="vh-location-button"]');
     await page.waitFor(200);
     await page.click('button[id="vh-location-button"]');
-    
+
     //type in requested Zip
     const zipCodeField = await page.waitForSelector('input[class="i_a ar_c i_c"]');
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         await zipCodeField.press('Backspace');
     }
     await zipCodeField.type(reqZip);
-    
+
     //click submit and wait for page to reload for new zipCode
     await page.waitForSelector('div[class="p_a p_c b_a b_q"] > button[id="zipcode-form-submit-button"]');
     page.click('div[class="p_a p_c b_a b_q"] > button[id="zipcode-form-submit-button"]');
@@ -105,11 +105,11 @@ async function updateZipCode(page, reqZip) {
 }
 
 //Constructs genericRetailerObj from product page html
-function getGenericObj (html, rank) {
+function getGenericObj(html, rank) {
     const $ = cheerio.load(html);
     var walmartObj = new Object();
     walmartObj.retailer = 'Walmart';
-    walmartObj.productName = $('[class="prod-ProductTitle font-normal"]').attr('content');    
+    walmartObj.productName = $('[class="prod-ProductTitle font-normal"]').attr('content');
     walmartObj.price = parseFloat($('span[class="price display-inline-block arrange-fit price price--stylized"] > span[class="visuallyhidden"]').text().substring(1));
     unitPriceObj = $('div[class="display-inline-block-xs valign-bottom prod-PaddingBottom--xxs"] > div').text();
     walmartObj.unitPrice = parseFloat(unitPriceObj.substring(1, 5));
