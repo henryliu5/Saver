ebd9890d244e134309a3742e4f07f59b1b287056
Henry <henry.liu.texas@gmail.com>
2020-05-22 16:28:54 -0500

    Fix retailers array brace

--- backend/scrape.js
+++ backend/scrape.js
@@ -3,7 +3,7 @@ const parseCsv = require('./parse');
 const kroger = require('./scrape/kroger');
 const target = require('./scrape/target');
 const walmart = require('./scrape/walmart');
-const retailers = [kroger, target, walmart;
+const retailers = [kroger, target, walmart];
 
 // 5/15/20
 // 2m 30s 75028
