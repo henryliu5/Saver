1452ddb36a2b1396ffb2038892d33c99d6666bfc
Henry <henry.liu.texas@gmail.com>
2020-08-23 20:23:05 -0500

    set zip in local storage on landing button press

--- client/components/Landing/Landing.js
+++ client/components/Landing/Landing.js
@@ -3,14 +3,15 @@ import { UpOutlined } from "@ant-design/icons";
 import { useState, useEffect } from "react";
 import styles from "./Landing.module.css";
 
-export default function Landing() {
+export default function Landing({ updateFunc }) {
   let [input, setInput] = useState(null);
+  let [text, setText] = useState("");
 
   // Focus on text box
   useEffect(() => {
-    console.log(input)
-    if(input != null)
-    input.focus();
+    if (input != null) {
+      input.focus();
+    }
   }, [input]);
 
   return (
@@ -18,12 +19,27 @@ export default function Landing() {
       <h1 className={styles.title}>Saver</h1>
       <h2 className={styles.subtitle}>Compare local grocers with one search</h2>
       <p className={styles.instruction}>Enter zip to get started</p>
-      <Input
-        placeholder="e.g. 75022"
-        bordered={true}
-        ref={(input) => setInput(input)}
+      <div className={styles.search}>
+        <Input
+          onChange={(e) => setText(e.target.value)}
+          style={{
+            textAlign: "center",
+            width: 150,
+          }}
+          placeholder="e.g. 75022"
+          bordered={true}
+          ref={(input) => setInput(input)}
+        />
+      </div>
+
+      <Button
+        className={styles.button}
+        type="primary"
+        disabled={text.length != 5}
+        shape="circle"
+        icon={<UpOutlined />}
+        onClick={()=>(updateFunc(text))}
       />
-      <Button type="primary" shape="circle" icon={<UpOutlined />} />
     </div>
   );
 }
