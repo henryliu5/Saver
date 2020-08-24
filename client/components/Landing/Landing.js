e7c01d10be7b0e5a9459150a0a91b09ec27060d2
Henry <henry.liu.texas@gmail.com>
2020-08-23 19:10:52 -0500

    add centering and basic text styling

--- client/components/Landing/Landing.js
+++ client/components/Landing/Landing.js
@@ -1,14 +1,15 @@
-import { Button, Space, Input, notification } from 'antd';
-import styles from './Landing.module.css';
+import { Button, Space, Input, notification } from "antd";
+import { UpOutlined } from "@ant-design/icons";
+import styles from "./Landing.module.css";
 
 export default function Landing() {
   return (
-    <div>
+    <div className={styles.main}>
       <h1 className={styles.title}>Saver</h1>
-      <p>Compare local grocers with one search</p>
-      <p>Enter zip to get started</p>
+      <h2 className={styles.subtitle}>Compare local grocers with one search</h2>
+      <p className={styles.instruction}>Enter zip to get started</p>
       <Input></Input>
-      <Button type="primary" shape="circle">a</Button>
+      <Button type="primary" shape="circle" icon={<UpOutlined />} />
     </div>
   );
 }
